cceb2393871d994863f09e096554518a5686f3d0
Mostofa Mohi Uddin Ahmed <mohiahmed931@gmail.com>
2026-07-18 17:08:41 +0600

    for email

--- src/main/java/com/sparktech/portfolio_deploy/Controller/ContactController.java
+++ src/main/java/com/sparktech/portfolio_deploy/Controller/ContactController.java
@@ -2,6 +2,9 @@ package com.sparktech.portfolio_deploy.Controller;
 
 import com.sparktech.portfolio_deploy.Model.Contact;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
